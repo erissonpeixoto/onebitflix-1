aa0fdec0f0be6179bc2a23a9581eec214687bc49
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-05-28 10:10:01 -0300

    Baixando o featured

--- app/javascript/packs/store/index.js
+++ app/javascript/packs/store/index.js
@@ -1,13 +1,14 @@
-
 import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
 
 import MovieMenu from './modules/movie_menu';
+import Watchable from './modules/watchable';
 
 const store = new Vuex.Store({
   modules: {
-    MovieMenu
+    MovieMenu,
+    Watchable
   }
 });
 
