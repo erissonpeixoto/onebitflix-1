a226251df398435daea291db26d198ee1e5d8a38
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-05-27 22:04:51 -0300

    Movie list component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './app.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'slick-carousel/slick/slick.css';
 import router from './routes.js';
 
 Vue.use(Vuetify)
